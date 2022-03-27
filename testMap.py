ffcc2a84c081c367cd433153f1ac60009e35c94d
yanweida <13115406799@163.com>
2022-03-27 15:06:59 +0800

    fix: 上传地图示例

--- testMap.py
+++ testMap.py
@@ -1,12 +1,15 @@
 import sys
 
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
 
 # 创建一个 application实例
 app = QApplication(sys.argv)
 win = QWidget()
-win.setWindowTitle('高德地图test')
+win.setWindowTitle('动物机器人-高德地图')
+app.setWindowIcon(QIcon('./images/mice2.jpg'))
 
 # 创建一个垂直布局器
 layout = QVBoxLayout()
@@ -14,73 +17,112 @@ win.setLayout(layout)
 
 # 创建一个 QWebEngineView 对象
 view = QWebEngineView()
-# view.load(QUrl(E:\miceRobot\gdmap.html))
+# view.load(QUrl("E:\miceRobot\walkingMap.html"))
 view.setHtml('''
- <!doctype html>
+<!doctype html>
 <html>
-
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="initial-scale=1.0, user-scalable=no, width=device-width">
-  <link rel="stylesheet" href="https://a.amap.com/jsapi_demos/static/demo-center/css/demo-center.css" />
-  <link rel="stylesheet" type="text/css" href="https://a.amap.com/jsapi_demos/static/demo-center/css/prety-json.css">
-  <style>
-    html,
-    body,
-    #container {
-      width: 100%;
-      height: 100%;
-    }
-  </style>
-  <title>获取搜索信息</title>
-</head>
-
-<body>
-  <div id="container"></div>
-  <div class="info">
-    <h4>搜索结果展示</h4>
-    <p><span id="input-info"></span></p>
-  </div>
-  <script src="https://webapi.amap.com/maps?v=1.4.15&key=c402e9f4b6dc87cf70727f8dd7f22dc8&plugin=AMap.PlaceSearch"></script>
-  <script type="text/javascript" src="https://a.amap.com/jsapi_demos/static/demo-center/js/jquery-1.11.1.min.js" ></script>
-  <script type="text/javascript" src="https://a.amap.com/jsapi_demos/static/demo-center/js/underscore-min.js" ></script>
-  <script type="text/javascript" src="https://a.amap.com/jsapi_demos/static/demo-center/js/backbone-min.js" ></script>
-  <script type="text/javascript" src='https://a.amap.com/jsapi_demos/static/demo-center/js/prety-json.js'></script>
-  <script>
-    // 初始化地图
-    var map = new AMap.Map("container", {
-        resizeEnable: true
-    });
-
-    // 获取搜索信息
-    function autoInput(){
-      var keywords = '北京大学';
-      AMap.plugin('AMap.PlaceSearch', function(){
-        var autoOptions = {
-          city: '全国'
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no, width=device-width">
+    <title>轨迹回放</title>
+    <link rel="stylesheet" href="https://a.amap.com/jsapi_demos/static/demo-center/css/demo-center.css"/>
+    <style>
+        html, body, #container {
+            height: 100%;
+            width: 100%;
         }
-        var placeSearch = new AMap.PlaceSearch(autoOptions);
-        placeSearch.search(keywords, function(status, result) {
-          // 搜索成功时，result即是对应的匹配数据
-          var node = new PrettyJSON.view.Node({
-              el: document.querySelector("#input-info"),
-              data: result
-          });
-        })
-      })
-    }
 
-    autoInput();
+        .input-card .btn{
+            margin-right: 1.2rem;
+            width: 9rem;
+        }
+
+        .input-card .btn:last-child{
+            margin-right: 0;
+        }
+    </style>
+</head>
+<body>
+<div id="container"></div>
+<div class="input-card">
+    <h4>轨迹回放控制</h4>
+    <div class="input-item">
+        <input type="button" class="btn" value="开始动画" id="start" onclick="startAnimation()"/>
+        <input type="button" class="btn" value="暂停动画" id="pause" onclick="pauseAnimation()"/>
+    </div>
+    <div class="input-item">
+        <input type="button" class="btn" value="继续动画" id="resume" onclick="resumeAnimation()"/>
+        <input type="button" class="btn" value="停止动画" id="stop" onclick="stopAnimation()"/>
+    </div>
+</div>
+<script type="text/javascript" src="https://webapi.amap.com/maps?v=2.0&key=c402e9f4b6dc87cf70727f8dd7f22dc8"></script>
+<script>
+    // JSAPI2.0 使用覆盖物动画必须先加载动画插件
+    AMap.plugin('AMap.MoveAnimation', function(){
+        var marker, lineArr = [[116.478935,39.997761],[116.478939,39.997825],[116.478912,39.998549],[116.478912,39.998549],[116.478998,39.998555],[116.478998,39.998555],[116.479282,39.99856],[116.479658,39.998528],[116.480151,39.998453],[116.480784,39.998302],[116.480784,39.998302],[116.481149,39.998184],[116.481573,39.997997],[116.481863,39.997846],[116.482072,39.997718],[116.482362,39.997718],[116.483633,39.998935],[116.48367,39.998968],[116.484648,39.999861]];
+
+        var map = new AMap.Map("container", {
+            resizeEnable: true,
+            center: [116.397428, 39.90923],
+            zoom: 17
+        });
+
+        marker = new AMap.Marker({
+            map: map,
+            position: [116.478935,39.997761],
+            icon: "https://a.amap.com/jsapi_demos/static/demo-center-v2/car.png",
+            offset: new AMap.Pixel(-13, -26),
+        });
+
+        // 绘制轨迹
+        var polyline = new AMap.Polyline({
+            map: map,
+            path: lineArr,
+            showDir:true,
+            strokeColor: "#28F",  //线颜色
+            // strokeOpacity: 1,     //线透明度
+            strokeWeight: 6,      //线宽
+            // strokeStyle: "solid"  //线样式
+        });
+
+        var passedPolyline = new AMap.Polyline({
+            map: map,
+            strokeColor: "#AF5",  //线颜色
+            strokeWeight: 6,      //线宽
+        });
 
 
-    
-    
+        marker.on('moving', function (e) {
+            passedPolyline.setPath(e.passedPath);
+            map.setCenter(e.target.getPosition(),true)
+        });
 
+        map.setFitView();
 
-  </script>
+        window.startAnimation = function startAnimation () {
+            marker.moveAlong(lineArr, {
+                // 每一段的时长
+                duration: 500,//可根据实际采集时间间隔设置
+                // JSAPI2.0 是否延道路自动设置角度在 moveAlong 里设置
+                autoRotation: true,
+            });
+        };
+
+        window.pauseAnimation = function () {
+            marker.pauseMove();
+        };
+
+        window.resumeAnimation = function () {
+            marker.resumeMove();
+        };
+
+        window.stopAnimation = function () {
+            marker.stopMove();
+        };
+    });
+</script>
 </body>
-
 </html>
 ''')
 
